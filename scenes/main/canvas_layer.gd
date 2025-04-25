f34cb1c750380da7ecc777d3a20d4797483a26d4
V Shree Harshitha <vsh9945@gmail.com>
2025-04-25 16:35:31 +0530

    Restructured  Forge_Biome

--- scenes/main/canvas_layer.gd
+++ scenes/main/canvas_layer.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 
 func _on_button_pressed():
 	print("Button clicked!")  # Test if working
-	get_tree().change_scene_to_file("res://scenes/environments/guardian_biome/guardian_biome.tscn")  # Load next scene
+	get_tree().change_scene_to_file("res://scenes/environments/forge_biome/map.tscn")  # Load next scene
 
 
 func _on_musictoggle_toggled(toggled_on: bool) -> void:
